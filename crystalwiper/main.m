1d5508b029bb9b3d1cca17df3fb54f4daec66986
Sven Anders Robbestad <robbestad@gmail.com>
2012-05-08 15:42:57 +0200

    Initial Commit

---
+++ crystalwiper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  crystalwiper
+//
+//  Created by Sven Anders Robbestad on 08.05.12.
+//  Copyright SOL 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
